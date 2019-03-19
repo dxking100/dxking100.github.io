20be1adf6995b275e74679f92f3ce05ae3868d7c
Victor lee <dxking100@126.com>
2019-03-19 18:09:27 +0800

    test
    test

---
+++ content/content.js
@@ -0,0 +1,40 @@
+
+
+var content = [
+	{
+		"id": 1,
+		"tie":1,
+		"name": "IDEA 项目上传到github",
+		"content":"",
+		"url":"file/IDEA 项目上传到github.mht",
+		"date":"2019-01-02"
+	},{
+		"id": 2,
+		"tie":1,
+		"name": "個人簡歷.mht",
+		"content":"",
+		"url":"file/a.mht",
+		"date":""
+	},{
+		"id": 3,
+		"tie":1,
+		"name": "ddd",
+		"content":"",
+		"url":"",
+		"date":""
+	},{
+		"id": 4,
+		"tie":1,
+		"name": "ddd",
+		"content":"",
+		"url":"",
+		"date":""
+	},{
+		"id": 5,
+		"tie":1,
+		"name": "ddd",
+		"content":"",
+		"url":"",
+		"date":""
+	}
+];
---
+++ content/type.js
@@ -0,0 +1,12 @@
+var type = [
+	{
+		"id": 1,
+		"name": "ddd"
+	},{
+		"id": 2,
+		"name": "ddd1"
+	},{
+		"id": 3,
+		"name": "ddd2"
+	}
+];
