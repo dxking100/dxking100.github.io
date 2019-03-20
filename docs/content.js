34612ea25177b3bcd3a1b30e03cecf29b95cea85
Victor lee <dxking100@126.com>
2019-03-20 10:47:39 +0800

    修改佈局

--- docs/content.js
+++ docs/content.js
@@ -1,40 +1,11 @@
-
-
+//-----------------------------配置分類
 var content = [
 	{
 		"id": 1,
-		"tie":1,
+		"tid":1,
 		"name": "啞鈴健身計劃",
 		"content":"",
 		"url":"file/sportplan.htm",
 		"date":"2018-02-07"
-	},{
-		"id": 2,
-		"tie":1,
-		"name": "個人簡歷.mht",
-		"content":"",
-		"url":"file/a.mht",
-		"date":""
-	},{
-		"id": 3,
-		"tie":1,
-		"name": "ddd",
-		"content":"",
-		"url":"",
-		"date":""
-	},{
-		"id": 4,
-		"tie":1,
-		"name": "ddd",
-		"content":"",
-		"url":"",
-		"date":""
-	},{
-		"id": 5,
-		"tie":1,
-		"name": "ddd",
-		"content":"",
-		"url":"",
-		"date":""
 	}
 ];
