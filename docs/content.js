c9312a8f098af69dd096325c6a3204ff50c2437c
Victor lee <dxking100@126.com>
2019-03-20 09:30:11 +0800

    添加健身計劃

--- docs/content.js
+++ docs/content.js
@@ -6,8 +6,8 @@ var content = [
 		"tie":1,
 		"name": "啞鈴健身計劃",
 		"content":"",
-		"url":"file/啞鈴健身計劃.html",
-		"date":"2019-01-02"
+		"url":"file/sportplan.htm",
+		"date":"2018-02-07"
 	},{
 		"id": 2,
 		"tie":1,
